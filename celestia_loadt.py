05ba0358c27b8551afea3e36961be0dc8db6b715
5 Elements Nodes <106030966+evangelionnight@users.noreply.github.com>
2023-05-12 13:02:50 +0200

    Update celestia_loadt.py

--- celestia_loadt.py
+++ celestia_loadt.py
@@ -7,11 +7,21 @@ import concurrent.futures
 
 parser = argparse.ArgumentParser()
 parser.add_argument("endpoint", help="the endpoint to call")
-parser.add_argument("-c", "--calls", metavar='', help="number of calls to make (default 100)", type=int, default=100)
-parser.add_argument("-t", "--threads", metavar='', help="number of threads to use (default 10)", type=int, default=10)
+parser.add_argument("-c", "--calls", metavar='', help="number of calls to make (default 1000)", type=int, default=1000)
+parser.add_argument("-t", "--threads", metavar='', help="number of threads to use (default 100)", type=int, default=100)
 parser.add_argument("-r", "--retry", metavar='', help="number of times to retry a failed call (default 0)", type=int, default=0)
 parser.add_argument("-a", "--auth", metavar='', help="authorization token for the API", required=True)
 args = parser.parse_args()
+
+num_calls = args.calls
+num_threads = args.threads
+retry_count = args.retry
+authorization = args.auth
+
+if num_calls <= 0:
+    raise ValueError("Number of calls must be greater than zero.")
+if num_threads <= 0:
+    raise ValueError("Number of threads must be greater than zero.")
 
 start = time.monotonic()
 
@@ -20,10 +30,6 @@ total_latency = 0
 total_errors = 0
 total_timeouts = 0
 
-num_calls = args.calls
-num_threads = args.threads
-retry_count = args.retry
-authorization = args.auth
 
 def make_request(i, stop_event):
     attempt = 0
@@ -37,8 +43,8 @@ def make_request(i, stop_event):
                 headers['Authorization'] = f'Bearer {authorization}'
             response = requests.post(args.endpoint,
                                      headers=headers,
-                                     json={'jsonrpc': '2.0', 'method': 'header.LocalHead', 'params': [], 'id': 1},
-                                     timeout=5)
+                                     json={'jsonrpc': '2.0', 'method': 'header.SyncState', 'params': [], 'id': 1},
+                                     timeout=15)
             end_time = time.monotonic()
             response.raise_for_status()
             data = response.json()
@@ -48,28 +54,37 @@ def make_request(i, stop_event):
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 401:
                 stop_event.set()
-            return None, None, e
-        except (requests.exceptions.RequestException, ValueError) as e:
+                return None, None, e
+            else:
+                print(f"HTTP error: {e}. Endpoint: {args.endpoint}.")
+                return None, None, e
+        except requests.exceptions.RequestException as e:
             if attempt == retry_count:
+                print(f"Request Exception error: {e}. Endpoint: {args.endpoint}.")
                 return None, None, e
             else:
                 attempt += 1
                 time.sleep(1)
                 if isinstance(e, requests.exceptions.ReadTimeout):
-                    return None, None, e
+                    print(f"ReadTimeout error: {e}. Endpoint: {args.endpoint}. Attempt: {attempt}.")
+                    continue
+                else:
+                    print(f"Request Exception error: {e}. Endpoint: {args.endpoint}. Attempt: {attempt}.")
+                    continue
+
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
     futures = [executor.submit(make_request, i, threading.Event()) for i in range(num_calls)]
 
     for i, future in enumerate(concurrent.futures.as_completed(futures)):
         try:
-            time_total, latency, error = future.result(timeout=5)
+            time_total, latency, error = future.result(timeout=15)
             if time_total:
                 total_time += time_total
                 total_latency += latency
                 if time_total >= 2000:
                     total_timeouts += 1
-                    print("Timeout error: waited for 5 seconds.")
+                    print("Timeout error: waited for 15 seconds.")
             if error:
                 print(f"Error: {error}")
                 total_errors += 1
@@ -80,7 +95,7 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         except requests.exceptions.ReadTimeout:
             print("\nTimeout error: read timed out.")
         except concurrent.futures.TimeoutError:
-            print("Timeout error: waited for 5 seconds.")
+            print("Timeout error: waited for 15 seconds.")
             total_timeouts += 1
         except Exception as e:
             print(f"Error: {e}")
